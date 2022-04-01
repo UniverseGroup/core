39fced3f10353f0f04c5ade1b780a511244abf93
popop098 <popop0982@naver.com>
2022-04-01 15:26:33 +0900

    ❤

--- pages/_app.js
+++ pages/_app.js
@@ -4,50 +4,13 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import '@fortawesome/fontawesome-svg-core/styles.css'
-// import dynamic from "next/dynamic";
 import {useRouter, Router} from 'next/router';
-import {useEffect} from 'react';
-import {useState} from "react";
-// import LoadingScreen from '../components/LoadingScreen';
 import NProgress from 'nprogress'
-// import {NextSeo} from "next-seo";
-// import dynamic from "next/dynamic";
-// const LoadingScreen = dynamic(() => import('../components/LoadingScreen'));
 NProgress.configure({ showSpinner: true })
 Router.events.on('routeChangeStart', NProgress.start)
 Router.events.on('routeChangeComplete', NProgress.done)
 Router.events.on('routeChangeError', NProgress.done)
 function MyApp({Component, pageProps}) {
-    // const router = useRouter();
-    // const [loading, setLoading] = useState(false);
-    // useEffect(()=>{
-    //   const handleStart = () => {
-    //       setLoading(true);
-    //   }
-    //   const handleStop = () => {
-    //       setLoading(false);
-    //   }
-
-    //   router.events.on('routeChangeStart', handleStart)
-    //   router.events.on('routeChangeComplete', handleStop)
-    //   router.events.on('routeChangeError', handleStop)
-
-    //   return () => {
-    //     router.events.off('routeChangeStart', handleStart)
-    //     router.events.off('routeChangeComplete', handleStop)
-    //     router.events.off('routeChangeError', handleStop)
-    //   }
-    // }, [router])
-    // return (
-    //     <>
-    //         {/* <NextSeo noindex={true} /> */}
-    //         {
-    //             loading ? (<LoadingScreen/>) : (<Component {...pageProps} />)
-    //         }
-
-    //     </>
-
-    // )
     return <Component {...pageProps} />
 }
 
