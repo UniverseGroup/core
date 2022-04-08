ef0c3cc475ee53eb5ca16bcdebbed1f7a9c09281
popop098 <popop0982@naver.com>
2022-04-09 00:15:54 +0900

    🐛 FIX: disabled nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -5,21 +5,21 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import {useRouter, Router} from 'next/router';
-import NProgress from 'nprogress'
+//import NProgress from 'nprogress'
 import * as gtag from '../lib/gtag'
 import { useEffect } from 'react';
-NProgress.configure({ showSpinner: true })
-Router.events.on('routeChangeStart', NProgress.start)
+//NProgress.configure({ showSpinner: true })
+//Router.events.on('routeChangeStart', NProgress.start)
 // Router.events.on('routeChangeComplete', ((url)=>{
 //     NProgress.done
 //     gtag.pageview(url)
 // }))
-Router.events.on('routeChangeError', NProgress.done)
+//Router.events.on('routeChangeError', NProgress.done)
 function MyApp({Component, pageProps}) {
     useEffect(()=>{
         const handleRouteChange = (url) => {
             gtag.pageview(url)
-            NProgress.done
+            //NProgress.done
           }
           Router.events.on('routeChangeComplete', handleRouteChange)
           return () => {
